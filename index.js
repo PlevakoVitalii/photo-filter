29d32387b6f6b142c77033500180a533fc291fa6
Виталий Плевако <60809089+PlevakoVitalii@users.noreply.github.com>
2021-04-26 16:23:30 +0300

    1 Add value in output forms

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
-const inputs = document.querySelectorAll('.filters input')
-const btnFullscreen = document.querySelector('.fullscreen');
-
-
+const inputs = document.querySelectorAll(".filters input");
+const outputs = document.querySelectorAll(".filters output");
+const btnFullscreen = document.querySelector(".fullscreen");
 
 /*FILTERS*/
-
 function handleUpdate() {
-  const suffix = this.dataset.sizin || '';
-  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+  const suffix = this.dataset.sizing || "";
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
+
+  //outputs[2].output.name[`--${this.name}`].innerHTML = this.value;
+  //alert(outputs[2].name);
+  //outputs.value = this.value;
+  //document.getElementById(id).innerHTML = this.value
 }
 
-inputs.forEach(input => input.addEventListener('change', handleUpdate));
-inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
-
+inputs.forEach((input) => input.addEventListener("input", handleUpdate));
+//outputs.forEach((input) => outputs.value = this.value;
+console.log(inputs);
+console.log(outputs);
 
 /* FOOLSCREEN */
-btnFullscreen.addEventListener("click", function (e) {
-  toggleFullScreen();
-}, false);
+btnFullscreen.addEventListener(
+  "click",
+  function (e) {
+    toggleFullScreen();
+  },
+  false
+);
 
 function toggleFullScreen() {
   if (!document.fullscreenElement) {
